ff70b50c0b8693822297a902fb84b475160e8e94
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-10-04 01:18:21 +0600

    event listener

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,41 @@
-let title =  document.querySelector("#description").value
+// Select UI Elements
+let description =  document.querySelector("#description").value
 let cost =  document.querySelector("#expense").value
+let formInput = document.querySelector("#expense-form")
 let balance = document.querySelector("#balance")
 let list = document.querySelector("#expense-list")
+let submitBtn = document.querySelector("button")
+let cancelEdit = document.querySelector("#cancelEditButton")
+let title = document.querySelector("#formTitle")
 
-document.querySelector("#expense-form").addEventListener("submit", event => {
-    event.preventDefault()
+// Initialize an array to store data in local storage
+let transition = JSON.parse(localStorage.getItem('transition')) || []
+let editTransitionId = null
+
+function handleTransction
 
 
 
-    document.querySelector("#expense-form").reset()
-})
+
+
+
+
+
+
+
+
+
+
+
+
+// event Listner
+formInput.addEventListener("submit", handleTransction)
+cancelEdit.addEventListener("click", cancelEditFn)
+
+// document.querySelector("#expense-form").addEventListener("submit", event => {
+//     event.preventDefault()
+//
+//
+//
+//     document.querySelector("#expense-form").reset()
+// })
