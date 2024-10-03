c91f1617bb6604ec67ff01a09b44174f12ef527c
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-10-04 00:56:25 +0600

    expense form created with css

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,11 @@
+let title =  document.querySelector("#description").value
+let cost =  document.querySelector("#expense").value
+let balance = document.querySelector("#balance")
+let list = document.querySelector("#expense-list")
+
 document.querySelector("#expense-form").addEventListener("submit", event => {
     event.preventDefault()
-    let title =  document.querySelector("#description").value
-    let cost =  document.querySelector("#expense").value
-    let total = document.querySelector("#totalExp")
-    let list = document.querySelector("#expense-list")
 
-    let value = {
-        title: title,
-        cost: cost
-    }
-    total.innerText= cost
-    list.forEach((item)=>{
-        item = `<li>${title} : ${cost} </li>`
-        return item
-    })
 
 
     document.querySelector("#expense-form").reset()
